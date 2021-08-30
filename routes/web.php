a5f044aa2829a027f2a52b518b446513767dda06
unknown <francisco>
2021-08-30 10:04:44 -0500

    Entregable

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
